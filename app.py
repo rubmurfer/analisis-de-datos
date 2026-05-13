f960e5236d20e50e3133c05739fe6b7643bab256
Rubén Murcia <rubenmurciafernandez8@gmail.com>
2026-05-13 13:08:47 +0200

    Primer cambio modular
    he añadido la carpeta utils/ para localizar los ficheros .py que asistirán a app.py

--- app.py
+++ app.py
@@ -1,24 +1,12 @@
-# Importamos las librerías necesarias
-from zipfile import ZipFile
-from pathlib import Path
-import pandas as pd
+# app.py
 
 from flask import Flask, render_template, redirect
+from utils.carga_datos import df_unidades, df_faltas, df_materias, df_matriculas, df_notas, pd
 
-datos = Path(__file__).parent.resolve() / "datos.zip" # Cargamos el fichero .zip desde el directorio actual.
-
-with ZipFile(datos) as z: # Creamos los dataframes en base a los ficheros del .zip con sus parámetros correspondientes
-    with z.open("datFaltas.csv") as f: df_faltas = pd.read_csv(f, encoding="latin-1", sep=";")
-    with z.open("datMatriculas.csv") as f: df_matriculas = pd.read_csv(f, encoding="latin-1", sep=";")
-    with z.open("datNotas.csv") as f: df_notas = pd.read_csv(f, encoding="latin-1", sep=";")
-    with z.open("datUnidades.csv") as f: df_unidades = pd.read_csv(f, encoding="latin-1", sep=";")
-    with z.open("materias_centro.csv") as f: df_materias_centro = pd.read_csv(f, encoding="latin-1", sep=";")
-    with z.open("materias_matriculas.csv") as f: df_materias_matriculas = pd.read_csv(f, encoding="latin-1", sep=",")
+print(df_faltas)
 
 
-print(df_notas)
-
-app = Flask(__name__)
+app = Flask(__name__) # Apartado de Web con Flask
 
 @app.route("/")
 def inicio():
---
+++ utils/carga_datos.py
@@ -0,0 +1,23 @@
+# utils/carga_datos.py
+
+# Importamos las librerías necesarias
+
+import pandas as pd
+from zipfile import ZipFile
+from pathlib import Path
+
+datos = Path(__file__).parent.resolve() / "datos.zip" # Cargamos el fichero .zip desde el directorio actual.
+
+with ZipFile(datos) as z: # Creamos los dataframes en base a los ficheros del .zip con sus parámetros correspondientes
+        with z.open("datNotas.csv") as f: df_notas = pd.read_csv(f, encoding="latin-1", sep=";")
+        with z.open("datMatriculas.csv") as f: df_matriculas = pd.read_csv(f, encoding="latin-1", sep=";")
+        with z.open("datFaltas.csv") as f: df_faltas = pd.read_csv(f, encoding="latin-1", sep=";")
+        with z.open("datMaterias.csv") as f: df_materias = pd.read_csv(f, encoding="latin-1", sep=";")
+        with z.open("datUnidades.csv") as f: df_unidades = pd.read_csv(f, encoding="latin-1", sep=";")
+
+# Cargamos solo las columnas necesarias
+df_notas = df_notas[["MATRICULA", "ANNO", "CURSO", "EVALUACION", "MATERIA", "CL_MATERIA", "NOTA"]]
+df_matriculas = df_matriculas[["MATRICULA", "ETAPA", "ANNO", "ESTUDIOS", "GRUPO", "ESTADOMATRICULA"]]
+df_faltas = df_faltas[["EXPEDIENTE", "ANNO", "FECHA_FALTA", "ESTUDIOS", "GRUPO", "MATERIA", "AUSENCIAS", "RETRASOS", "JUSTIFICADAS"]]
+df_materias = df_materias[["CODIGO", "DESCRIPCION", "ABREVIATURA"]]
+df_unidades = df_unidades[["ANNO", "GRUPO", "ESTUDIO", "CURSO"]] # Opcional. Falta "TUTOR"
